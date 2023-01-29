49262d3a2e97bec35e0e1e641b9b36bd92ebba7c
ADARSH ANAND <123612161+Badnamdakku@users.noreply.github.com>
2023-01-29 08:54:00 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "24585748"))
     API_HASH = os.environ.get("API_HASH", "7028654784d762563b9c3ca6f370cefc")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5689681976:AAFE4FLueeHZGU6sTN7IE6s0NUT0bbqXywM")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOMABu0CKgy_EVeoG-Nkygcld2PjHR8kbcZMTn1NqJ6tYT_ytPSwzAcXNdWE53LJUr2UP0Jzjzv0azv67PaXBMGAO7xAu1KL5CT0ze5pmABGvMGgGK16jkbq6qndqnS1rjYW3VXB8RTmN5VD3jVzzabH1BSO0QyfDF18VskN552rK6_yp0C7aF_x7oH_ve2gKxYorhO-dcFmBhtEVGA_A9QfwWtaCGtoPoevABQrmYfZ53kC1Q9pLYrRk7X6tmy6EFksUpYy8vOBW7vWBcP3P3GCcsLYbuCDw25ceo7lndm9N2-OahKTo3QXbSy7f36eKG5_QjAZK1_zD2ZwfG856XKRnIWU=")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6132850724:AAF-SR-ezgOJe3S9e0Zru6a9StiKah2sAHQ")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGYBu1n--5QtTC-VoqfGAz-nH6pUL1E5TQGhdFDOs9s6j8kpOqrTJMTijWq4n_xDNUeJSdQJ8ieBNAimWw8vp_fLXnOCzAlNzFzTqoxqRiW553qmMlmaZMY6232lX68Cb4lNj-MHSC78f5C1KQ4HKmcemB0mw6_CNgEbdl_tAgLg68CvXFWZK5FyOZiS4Avu3KaYDj64C4809AI9DdTMOjkIZMcP3gcDU_H4LnB11WTl_6Xa_590l9Wdw5En_hyw2hKxVOUwaaNgU6lqYn99NVVHd48k6G0QDxutNXkaCYDWZsKCZaI_TRkaCNql57lIG1fOIGUgltDgUiM3qwyZviUvOp8=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Dj_badnam_bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Dj_badnam_op_bot")
     SUPPORT = os.environ.get("SUPPORT", "Top_eagle_fighter1") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "badnam_dakku") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/022e1ffe04f885c3fcd2c.jpg")
